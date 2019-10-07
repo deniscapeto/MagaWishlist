7713a8daf951495e426287acf440a600256476d6
Denis Capeto <denis.capeto@luizalabs.com>
2019-10-07 07:47:38 -0300

    Adding option to configure api configurations using environment variables

--- MagaWishlist/Startup.cs
+++ MagaWishlist/Startup.cs
@@ -33,12 +33,18 @@ namespace MagaWishlist
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
-
         public IConfiguration Configuration { get; }
+
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+                .AddEnvironmentVariables();
+
+            Configuration = builder.Build();
+        }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
